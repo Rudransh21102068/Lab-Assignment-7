d4955633c608aa8371eb7ced7b34e21bc0630821
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-07-16 23:43:10 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,20 @@
+======= RESTART: C:\Users\ashas\OneDrive\Documents\ASSIGNMENT 7\Ques 4.py ======
+Enter the number of students :4
+Enter the marks of student: 45
+Enter the marks of student: 35
+Enter the marks of student: 92
+Enter the marks of student: 75
+Given array is
+45 35 92 75 
+Sorted array is: 
+35 45 75 92
+
+======= RESTART: C:\Users\ashas\OneDrive\Documents\ASSIGNMENT 7\Ques 5.py ======
+Enter the numbers of list: 5 6 9 19 36 78 4 9 6 13 0 12 4 9 6 78 6 34 35 56 9 4
+Enter the number to search for repeatition 4
+Sorted Array is, [0, 4, 4, 4, 5, 6, 6, 6, 6, 9, 9, 9, 9, 12, 13, 19, 34, 35, 36, 56, 78, 78]
+Number of occurences of element 4 is: 3
+
+======= RESTART: C:\Users\ashas\OneDrive\Documents\ASSIGNMENT 7\Ques 6.py ======
+Sorted Array in Ascending Order: 
+[2, 4, 5, 10, 20]
